e3e151bee8910f99aca6a65e15a40adc89564613
Ken <ken.Yu@mile.cloud>
2021-01-30 11:18:24 +0800

    Revise path

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Head from 'next/head'
+import Head from "next/head"
 
 import DefaultLayout from "@/layout/DefaultLayout";
 
@@ -9,17 +9,17 @@ const IndexPage = props => {
       <Head>
         <title>Ken Blog</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta charset='utf-8' />
-        <meta http-equiv='X-UA-Compatible' content='IE=edge' />
-        <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
-        <meta name='description' content='Description' />
-        <meta name='keywords' content='Keywords' />
-        <meta name='theme-color' content='#317EFB' />
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+        <meta name="description" content="Js, React and GCP blog" />
+        <meta name="keywords" content="engineer" />
+        <meta name="theme-color" content="#317EFB" />
 
-        <link rel='manifest' href='/manifest.json' />
-        <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
-        <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
-        <link href='/apple-icon.png' rel='apple-touch-icon'></link>
+        <link rel="manifest" href="/manifest.json" />
+        <link href="/icons/favicon-16x16-dunplab-manifest-28429.png" rel="icon" type="image/png" sizes="16x16" />
+        <link href="/icons/favicon-32x32-dunplab-manifest-28429.png" rel="icon" type="image/png" sizes="32x32" />
+        <link href="/icons/apple-icon-60x60-dunplab-manifest-28429.png" rel="apple-touch-icon" type="image/png" sizes="60x60" />
       </Head>
       <DefaultLayout />
     </div>
