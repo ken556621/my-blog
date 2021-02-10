ed57a09e085b2e03026c82654a243cf25ce8e7d9
Ken <ken.Yu@mile.cloud>
2021-02-10 16:12:26 +0800

    Add meta, alt and title in each page

--- [category]/[slug].js
+++ [category]/[slug].js
@@ -25,7 +25,18 @@ const Blog = (props) => {
       <div>
         <Head>
             <title>{getTitle(blog.content)}</title>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+            <meta charset="utf-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+            <meta name="description" content={getTitle(blog.content)} />
+            <meta name="keywords" content="engineer" />
+            <meta name="theme-color" content="#317EFB" />
+
+            <link rel="manifest" href="/manifest.json" />
+            <link href="/icons/favicon-16x16-dunplab-manifest-28429.png" rel="icon" type="image/png" sizes="16x16" />
+            <link href="/icons/favicon-32x32-dunplab-manifest-28429.png" rel="icon" type="image/png" sizes="32x32" />
+            <link href="/icons/apple-icon-60x60-dunplab-manifest-28429.png" rel="apple-touch-icon" type="image/png" sizes="60x60" />
         </Head>
         <DefaultLayout />
         <SideList
