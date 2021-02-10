36b4b6e8acf6af719589eae87ac3eb9088d0e36a
Ken <ken.Yu@mile.cloud>
2021-02-10 16:12:26 +0800

    Add meta, alt and title in each page

--- pages/[category]/index.js
+++ pages/[category]/index.js
@@ -1,5 +1,6 @@
 
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 import DefaultLayout from "@/layout/DefaultLayout";
 
@@ -34,6 +35,20 @@ const List = (props) => {
     if(isHomePage){
         return (
             <>
+                <Head>
+                    <title>Ken Code</title>
+                    <meta charset="utf-8" />
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+                    <meta name="description" content="Ken Code blog" />
+                    <meta name="keywords" content="engineer" />
+                    <meta name="theme-color" content="#317EFB" />
+
+                    <link rel="manifest" href="/manifest.json" />
+                    <link href="/icons/favicon-16x16-dunplab-manifest-28429.png" rel="icon" type="image/png" sizes="16x16" />
+                    <link href="/icons/favicon-32x32-dunplab-manifest-28429.png" rel="icon" type="image/png" sizes="32x32" />
+                    <link href="/icons/apple-icon-60x60-dunplab-manifest-28429.png" rel="apple-touch-icon" type="image/png" sizes="60x60" />
+                </Head>
                 <DefaultLayout />
                 <Home />
             </>
@@ -42,6 +57,21 @@ const List = (props) => {
 
     return (
         <>
+            <Head>
+                <title>{"Ken Code " + category.toUpperCase()}</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta charset="utf-8" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+                <meta name="description" content="Js, React and GCP blog" />
+                <meta name="keywords" content="engineer" />
+                <meta name="theme-color" content="#317EFB" />
+
+                <link rel="manifest" href="/manifest.json" />
+                <link href="/icons/favicon-16x16-dunplab-manifest-28429.png" rel="icon" type="image/png" sizes="16x16" />
+                <link href="/icons/favicon-32x32-dunplab-manifest-28429.png" rel="icon" type="image/png" sizes="32x32" />
+                <link href="/icons/apple-icon-60x60-dunplab-manifest-28429.png" rel="apple-touch-icon" type="image/png" sizes="60x60" />
+            </Head>
             <DefaultLayout />
             <SideList
                 category={category}
