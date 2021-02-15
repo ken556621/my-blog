3911ae74fef2157e17b78f88ca9699080e172dfb
Ken <ken.Yu@mile.cloud>
2021-02-15 19:34:14 +0800

    Refactor and update author info

--- [category]/[slug].js
+++ [category]/[slug].js
@@ -1,7 +1,6 @@
-import Head from "next/head"
 
+import Header from "@/components/Header";
 import SideList from "@/components/SideList";
-import DefaultLayout from "@/layout/DefaultLayout";
 
 import styles from "@/styles/blog.module.scss";
 
@@ -23,40 +22,26 @@ const Blog = (props) => {
 
     return (
       <div>
-        <Head>
-            <title>{getTitle(blog.content) + " | Ken Code Blog"}</title>
-
-            <meta charset="utf-8" />
-            <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1" />
-            <meta name="description" content="談到學程式，有常見的3大迷思，非本科、跨領域無法學好程式？學了程式是不是只能當工程師？其實你能培養第二專長，成為T型人才。這個部落格紀錄自學轉職過程中的所學所聞。" />
-            <meta content="從體能教練轉職前端工程師、不斷自學精進和熱愛用技術去解決身邊的問題｜Ken Code Blog" property="og:title" />
-            <meta content="談到學程式，有常見的3大迷思，非本科、跨領域無法學好程式？學了程式是不是只能當工程師？其實你能培養第二專長，成為T型人才。這個部落格紀錄自學轉職過程中的所學所聞。" property="og:description" />
-            <meta content="從體能教練轉職前端工程師、不斷自學精進和熱愛用技術去解決身邊的問題｜Ken Code Blog" property="twitter:title" />
-            <meta content="談到學程式，有常見的3大迷思，非本科、跨領域無法學好程式？學了程式是不是只能當工程師？其實你能培養第二專長，成為T型人才。這個部落格紀錄自學轉職過程中的所學所聞。" property="twitter:description" />
-            <meta name="keywords" content="前端工程師、轉職、自學、體能教練" />
-            <meta name="theme-color" content="#317EFB" />
-
-            <link rel="manifest" href="/manifest.json" />
-            <link href="/icons/favicon-16x16-dunplab-manifest-28429.png" rel="icon" type="image/png" sizes="16x16" />
-            <link href="/icons/favicon-32x32-dunplab-manifest-28429.png" rel="icon" type="image/png" sizes="32x32" />
-            <link href="/icons/apple-icon-60x60-dunplab-manifest-28429.png" rel="apple-touch-icon" type="image/png" sizes="60x60" />
-        </Head>
-        <DefaultLayout />
+        <Header
+            title = {`${getTitle(blog.content)} | 游肯扣部落格`}
+            description = {`肯游扣部落格關於${getTitle(blog.content)}的介紹`}
+            sharingTitle = {`肯游扣部落格關於${getTitle(blog.content)}的介紹`}
+        />
         <SideList
           category={category}
           list={allFormatBlogs}
         />
-        <div className={styles.container}>
+        <article className={styles.container}>
           <section
             className={styles.articleSection}
             dangerouslySetInnerHTML={{ __html: blog.content }}
           />
-        </div>
+        </article>
       </div>
     )
   }
 
-  export async function getStaticProps(context) {
+  export const getStaticProps = async context => {
     const fs = require("fs");
 
     const { category, slug } = context.params;
@@ -106,7 +91,7 @@ const Blog = (props) => {
     };
   }
 
-  export async function getStaticPaths() {
+  export const getStaticPaths = async () => {
     const fs = require("fs");
 
     const folderPath = `${process.cwd()}/contents/`;
--- index.js
+++ index.js
@@ -1,30 +1,22 @@
 import React from "react";
-import Head from "next/head";
 
-import DefaultLayout from "@/layout/DefaultLayout";
+import Header from "@/components/Header";
+
+import styles from "@/styles/home.module.scss";
 
 const IndexPage = props => {
   return (
-    <div>
-      <Head>
-        <title>Ken Code Blog</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta charset="utf-8" />
-        <meta name="description" content="談到學程式，有常見的3大迷思，非本科、跨領域無法學好程式？學了程式是不是只能當工程師？其實你能培養第二專長，成為T型人才。這個部落格紀錄自學轉職過程中的所學所聞。" />
-        <meta content="從體能教練轉職前端工程師、不斷自學精進和熱愛用技術去解決身邊的問題｜Ken Code Blog" property="og:title" />
-        <meta content="談到學程式，有常見的3大迷思，非本科、跨領域無法學好程式？學了程式是不是只能當工程師？其實你能培養第二專長，成為T型人才。這個部落格紀錄自學轉職過程中的所學所聞。" property="og:description" />
-        <meta content="從體能教練轉職前端工程師、不斷自學精進和熱愛用技術去解決身邊的問題｜Ken Code Blog" property="twitter:title" />
-        <meta content="談到學程式，有常見的3大迷思，非本科、跨領域無法學好程式？學了程式是不是只能當工程師？其實你能培養第二專長，成為T型人才。這個部落格紀錄自學轉職過程中的所學所聞。" property="twitter:description" />
-        <meta name="keywords" content="前端工程師、轉職、自學、體能教練" />
-        <meta name="theme-color" content="#317EFB" />
-
-        <link rel="manifest" href="/manifest.json" />
-        <link href="/icons/favicon-16x16-dunplab-manifest-28429.png" rel="icon" type="image/png" sizes="16x16" />
-        <link href="/icons/favicon-32x32-dunplab-manifest-28429.png" rel="icon" type="image/png" sizes="32x32" />
-        <link href="/icons/apple-icon-60x60-dunplab-manifest-28429.png" rel="apple-touch-icon" type="image/png" sizes="60x60" />
-      </Head>
-      <DefaultLayout />
-    </div>
+    <>
+      <Header />
+      <div className={styles.container}>
+        <h1 className={styles.title}>
+          游肯扣的部落格 | Yu Ken Code Blog
+        </h1>
+        <div className={styles.subTitle}>
+          原本體能教練轉職前端工程師隨便寫寫紀錄學習歷程，相信人沒辦法拒絕科技的迭代，而沒有人，科技也會變得毫無意義。喜歡跟朋友討論軟體開發和一言不合就寫扣的乾脆。
+        </div>
+      </div>
+    </>
   );
 }
 
