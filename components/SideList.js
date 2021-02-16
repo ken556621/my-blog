3d772a2ce6a6a77b9fe0468b88d584e6123632cb
Ken <ken.Yu@mile.cloud>
2021-02-16 18:19:16 +0800

    Fix side bar bug

--- components/SideList.js
+++ components/SideList.js
@@ -7,9 +7,6 @@ import ListItemText from "@material-ui/core/ListItemText";
 import styles from "@/styles/sideList.module.scss"
 
 
-
-
-
 const SideList = props => {
     const {
         category = "",
@@ -20,40 +17,52 @@ const SideList = props => {
         return <>No Article</>
     }
 
-    const getAllTitle = () => {
+    const getTitle = content => {
         const titleRole = new RegExp("<h1>.+?</h1>");
 
-        const allTitle = list.map(blog => blog.match(titleRole)[0].replace("<h1>", "").replace("</h1>", ""));
+        const title = content.match(titleRole)[0].replace("<h1>", "").replace("</h1>", "");
 
-        return allTitle
+        return title
+      }
+
+    const getPath = content => {
+        const titleRole = new RegExp("<p>tags:.+?</p>");
+
+        const path = content.match(titleRole)[0].replace("<p>tags:", "").replace("</p>", "").toLowerCase();
+
+        return path
+    }
+
+    const CustomListItem = () => {
+        return (
+            list.map((content, index) => (
+                <ListItem
+                    key={index}
+                    className={styles.listItemRoot}
+                    button
+                >
+                    <Link
+                        key={index}
+                        href={{
+                            pathname: '/[category]/[title]',
+                            query: { category, title: getPath(content) },
+                        }}
+                    >
+                        <a>
+                            <ListItemText
+                                primary={getTitle(content)}
+                            />
+                        </a>
+                    </Link>
+                </ListItem>
+            ))
+        )
     }
 
     return (
         <div className={styles.container}>
             <List dense disablePadding>
-                {
-                    getAllTitle().map((title, index) => (
-
-                        <ListItem
-                            className={styles.listItemRoot}
-                            button
-                        >
-                            <Link
-                                key={index}
-                                href={{
-                                    pathname: '/[category]/[title]',
-                                    query: { category, title: title.toLowerCase() },
-                                }}
-                            >
-                                <a>
-                                    <ListItemText
-                                        primary={title}
-                                    />
-                                </a>
-                            </Link>
-                        </ListItem>
-                    ))
-                }
+                <CustomListItem />
             </List>
         </div>
     )
