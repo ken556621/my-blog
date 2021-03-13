0c793d35e40ac31656c461af7b8d160dcc2415fa
Ken <ken.Yu@mile.cloud>
2021-03-13 15:57:38 +0800

    Add context

--- [category]/[slug].js
+++ [category]/[slug].js
@@ -1,4 +1,3 @@
-
 import Header from "@/components/Header";
 import SideList from "@/components/SideList";
 import Banner from "@/components/Banner";
@@ -7,145 +6,145 @@ import styles from "@/styles/blog.module.scss";
 
 
 const Blog = props => {
-    const {
-      category = "",
-      allFormatBlogs = [],
-      blog = {}
-    } = props;
+  const {
+    category = "",
+    allFormatBlogs = [],
+    blog = {}
+  } = props;
 
-    const getTitle = content => {
-      const titleRole = new RegExp("<h1>.+?</h1>");
+  const getTitle = content => {
+    const titleRole = new RegExp("<h1>.+?</h1>");
 
-      if(!titleRole){
-          return ""
-      }
-
-      const title = content.match(titleRole)[0].replace("<h1>", "").replace("</h1>", "");
-
-      return title
+    if (!titleRole) {
+      return ""
     }
 
-    const getDate = content => {
-      const dateRole = new RegExp("<p>date:.+?</p>");
+    const title = content.match(titleRole)[0].replace("<h1>", "").replace("</h1>", "");
 
-      if(!dateRole){
-          return ""
-      }
-
-      const date = content.match(dateRole)[0].replace("<p>date:", "").replace("</p>", "");
-
-      return date
-    }
-
-    return (
-      <div>
-        <Header
-            title = {`${getTitle(blog.content)} | 游肯扣部落格`}
-            description = {`肯游扣部落格關於${getTitle(blog.content)}的介紹`}
-            sharingTitle = {`肯游扣部落格關於${getTitle(blog.content)}的介紹`}
-        />
-        <Banner
-            word = {`${getTitle(blog.content)}`}
-            date = {getDate(blog.content)}
-        />
-        <SideList
-          category={category}
-          list={allFormatBlogs}
-        />
-        <article className={styles.container}>
-          <section
-            className={styles.articleSection}
-            dangerouslySetInnerHTML={{ __html: blog.content }}
-          />
-        </article>
-      </div>
-    )
+    return title
   }
 
-  export const getStaticProps = async context => {
-    const fs = require("fs");
+  const getDate = content => {
+    const dateRole = new RegExp("<p>date:.+?</p>");
 
-    const { category, slug } = context.params;
-    const path = `${process.cwd()}/contents/${category}/${slug}.md`;
+    if (!dateRole) {
+      return ""
+    }
 
-    const hljs = require("highlight.js");
+    const date = content.match(dateRole)[0].replace("<p>date:", "").replace("</p>", "");
 
-    const md = require("markdown-it")({
-      html: true,
-      highlight: (str, lang) => {
-        if (lang && hljs.getLanguage(lang)) {
-          try {
-            return hljs.highlight(lang, str).value;
-          } catch (__) {
+    return date
+  }
 
-          }
+  return (
+    <div>
+      <Header
+        title={`${getTitle(blog.content)} | 游肯扣部落格`}
+        description={`肯游扣部落格關於${getTitle(blog.content)}的介紹`}
+        sharingTitle={`肯游扣部落格關於${getTitle(blog.content)}的介紹`}
+      />
+      <Banner
+        word={`${getTitle(blog.content)}`}
+        date={getDate(blog.content)}
+      />
+      <SideList
+        category={category}
+        list={allFormatBlogs}
+      />
+      <article className={styles.container}>
+        <section
+          className={styles.articleSection}
+          dangerouslySetInnerHTML={{ __html: blog.content }}
+        />
+      </article>
+    </div>
+  )
+}
+
+export const getStaticProps = async context => {
+  const fs = require("fs");
+
+  const { category, slug } = context.params;
+  const path = `${process.cwd()}/contents/${category}/${slug}.md`;
+
+  const hljs = require("highlight.js");
+
+  const md = require("markdown-it")({
+    html: true,
+    highlight: (str, lang) => {
+      if (lang && hljs.getLanguage(lang)) {
+        try {
+          return hljs.highlight(lang, str).value;
+        } catch (__) {
+
         }
-
-        return "";
       }
-    });
 
-    const rawContent = fs.readFileSync(path, {
-      encoding: "utf-8",
-    });
+      return "";
+    }
+  });
 
-    const files = fs.readdirSync(`${process.cwd()}/contents/${category}`, "utf-8");
+  const rawContent = fs.readFileSync(path, {
+    encoding: "utf-8",
+  });
 
-    const allMdFiles = files.filter(fn => fn.endsWith(".md"));
+  const files = fs.readdirSync(`${process.cwd()}/contents/${category}`, "utf-8");
 
-    const allContent = allMdFiles.map(mdFile => fs.readFileSync(`${process.cwd()}/contents/${category}/${mdFile}`, {
-        encoding: "utf-8",
-    }));
+  const allMdFiles = files.filter(fn => fn.endsWith(".md"));
 
-    const allFormatBlogs = allContent.map(content => md.render(content));
+  const allContent = allMdFiles.map(mdFile => fs.readFileSync(`${process.cwd()}/contents/${category}/${mdFile}`, {
+    encoding: "utf-8",
+  }));
 
-    const result = md.render(rawContent);
+  const allFormatBlogs = allContent.map(content => md.render(content));
+
+  const result = md.render(rawContent);
+
+  return {
+    props: {
+      category,
+      allFormatBlogs,
+      blog: {
+        content: result
+      }
+    },
+  };
+}
+
+export const getStaticPaths = async () => {
+  const fs = require("fs");
+
+  const folderPath = `${process.cwd()}/contents/`;
+  const files = fs.readdirSync(folderPath, "utf-8");
+
+  const result = files.map(folder => {
+    const blogPath = `${process.cwd()}/contents/${folder}`;
+    const blog = fs.readdirSync(blogPath, "utf-8");
 
     return {
-      props: {
-          category,
-          allFormatBlogs,
-          blog: {
-            content: result
+      folder,
+      blogs: blog
+    }
+  })
+
+  const paths = [];
+
+  result.forEach(file =>
+    file.blogs.forEach(blog => {
+      paths.push({
+        params: {
+          category: file.folder,
+          slug: blog.replace(".md", "")
         }
-      },
-    };
-  }
-
-  export const getStaticPaths = async () => {
-    const fs = require("fs");
-
-    const folderPath = `${process.cwd()}/contents/`;
-    const files = fs.readdirSync(folderPath, "utf-8");
-
-    const result = files.map(folder => {
-      const blogPath = `${process.cwd()}/contents/${folder}`;
-      const blog = fs.readdirSync(blogPath, "utf-8");
-
-      return {
-        folder,
-        blogs: blog
-      }
-    })
-
-    const paths = [];
-
-    result.forEach(file =>
-      file.blogs.forEach(blog => {
-        paths.push({
-          params: {
-            category: file.folder,
-            slug: blog.replace(".md", "")
-          }
-        })
       })
-    )
+    })
+  )
 
-    return {
-      paths,
-      fallback: false,
-    };
-  }
+  return {
+    paths,
+    fallback: false,
+  };
+}
 
-  export default Blog
+export default Blog
 
--- _app.js
+++ _app.js
@@ -1,9 +1,55 @@
+import { useState } from "react";
+
+import getConfig from "next/config";
+
 import styles from "@/styles/global.scss"
 
-const MyApp = ({ Component, pageProps }) => {
+import firebase from "firebase/app";
+import "firebase/messaging";
+
+import { ThemeContext, themes } from "@/context/themeContext";
+import { DrawerContext, drawer } from "@/context/drawerContext";
+
+
+const MyApp = ({ Component, pageProps, config }) => {
+  const [backgroundColor, setBackgroundColor] = useState(themes.light);
+  const [drawerOpened, setDrawerOpened] = useState(drawer.isOpened);
+
   return (
-    <Component {...pageProps} />
+    <ThemeContext.Provider value={{ backgroundColor, setBackgroundColor }}>
+      <DrawerContext.Provider value={{ drawerOpened, setDrawerOpened }}>
+        <Component {...pageProps} />
+      </DrawerContext.Provider>
+    </ThemeContext.Provider>
   )
+}
+
+MyApp.getInitialProps = async () => {
+  const { serverRuntimeConfig } = getConfig();
+  const {
+    FIREBASE_API_KEY,
+    FIREBASE_APP_ID,
+    FIREBASE_PROJECT_ID,
+    FIREBASE_SENDER_ID,
+    STORAGEBUCKET,
+    AUTHDOMAIN,
+    MEASUREMENTID
+  } = serverRuntimeConfig;
+
+  const firebaseConfig = {
+    apiKey: `${FIREBASE_API_KEY}`,
+    appId: `${FIREBASE_APP_ID}`,
+    authDomain: `${FIREBASE_PROJECT_ID}.firebaseapp.com`,
+    databaseURL: `https://${AUTHDOMAIN}`,
+    messagingSenderId: FIREBASE_SENDER_ID,
+    projectId: `${FIREBASE_PROJECT_ID}`,
+    storageBucket: `${STORAGEBUCKET}`,
+    measurementId: `${MEASUREMENTID}`
+  };
+
+  return {
+    config: firebaseConfig
+  }
 }
 
 export default MyApp
