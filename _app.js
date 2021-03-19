cf7ade4d06e13383325c07ec05e694fe8f6ac526
Ken <ken.Yu@mile.cloud>
2021-03-20 01:25:45 +0800

    Remove firebase config

--- _app.js
+++ _app.js
@@ -1,11 +1,6 @@
 import { useState, useEffect } from "react";
 
-import getConfig from "next/config";
-
 import styles from "@/styles/global.scss"
-
-import firebase from "firebase/app";
-import "firebase/messaging";
 
 import { ThemeProvider } from "@material-ui/styles";
 
@@ -35,35 +30,6 @@ const MyApp = ({ Component, pageProps, config }) => {
       </DrawerContext.Provider>
     </DarkModeContext.Provider>
   )
-}
-
-MyApp.getInitialProps = async (ctx) => {
-  const { serverRuntimeConfig } = getConfig();
-
-  const {
-    FIREBASE_API_KEY,
-    FIREBASE_APP_ID,
-    FIREBASE_PROJECT_ID,
-    FIREBASE_SENDER_ID,
-    STORAGEBUCKET,
-    AUTHDOMAIN,
-    MEASUREMENTID
-  } = serverRuntimeConfig;
-
-  const firebaseConfig = {
-    apiKey: `${FIREBASE_API_KEY}`,
-    appId: `${FIREBASE_APP_ID}`,
-    authDomain: `${FIREBASE_PROJECT_ID}.firebaseapp.com`,
-    databaseURL: `https://${AUTHDOMAIN}`,
-    messagingSenderId: FIREBASE_SENDER_ID,
-    projectId: `${FIREBASE_PROJECT_ID}`,
-    storageBucket: `${STORAGEBUCKET}`,
-    measurementId: `${MEASUREMENTID}`
-  };
-
-  return {
-    config: firebaseConfig
-  }
 }
 
 export default MyApp
