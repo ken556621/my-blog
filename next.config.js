08debd23e933f8b7e4f929d28c8c1ee64c6ca547
Ken <ken.Yu@mile.cloud>
2021-03-24 13:31:10 +0800

    Remove next pwa thrid party

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
-const withPWA = require("next-pwa");
 
-module.exports = withPWA(
-  {
-    target: "serverless",
-    webpack: config => {
-      config.module.rules.push({ test: /\.md$/, use: "raw-loader" })
-      config.module.rules.push({ test: /\.yml$/, use: "raw-loader" })
-      return config
-    },
-    pwa: {
-      dest: "public"
-    }
+
+module.exports = {
+  target: "serverless",
+  webpack: config => {
+    config.module.rules.push({ test: /\.md$/, use: "raw-loader" })
+    config.module.rules.push({ test: /\.yml$/, use: "raw-loader" })
+    return config
   }
-)
+}
