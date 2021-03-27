d27fc87430c039103779ba79f5917c931197dfe8
Ken <ken.Yu@mile.cloud>
2021-03-27 22:55:00 +0800

    Remove pwa third party

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 const withPWA = require("next-pwa")
 
-module.exports = withPWA({
+module.exports = {
   target: "serverless",
   assetPrefix: "/KBlog/",
   webpack: config => {
     config.module.rules.push({ test: /\.md$/, use: "raw-loader" })
     config.module.rules.push({ test: /\.yml$/, use: "raw-loader" })
     return config
-  },
-  pwa: {
-    dest: "public"
   }
-})
+}
