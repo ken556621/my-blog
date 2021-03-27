35804f5855d22ad884ab095d213a2a20288ceaa0
Ken <ken.Yu@mile.cloud>
2021-03-27 10:09:59 +0800

    Back install pwa next third party

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
+const withPWA = require("next-pwa")
 
 
-module.exports = {
+module.exports = withPWA({
   target: "serverless",
   webpack: config => {
     config.module.rules.push({ test: /\.md$/, use: "raw-loader" })
     config.module.rules.push({ test: /\.yml$/, use: "raw-loader" })
     return config
+  },
+  pwa: {
+    dest: "public"
   }
-}
+})
