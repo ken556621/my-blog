8f70018357c5df05357b275283d76567ee6cf44e
ken <ken556621@gmail.com>
2021-08-12 22:06:30 +0800

    Modified navigation style

--- components/Header.js
+++ components/Header.js
@@ -1,136 +1,161 @@
-import { useContext } from "react"
+import { useContext } from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import clsx from "clsx";
 
 import { makeStyles } from "@material-ui/core";
 import { Grid, Button, Toolbar, Avatar } from "@material-ui/core";
-import IconButton from '@material-ui/core/IconButton';
-import Brightness4Icon from '@material-ui/icons/Brightness4';
-import Brightness4OutlinedIcon from '@material-ui/icons/Brightness4Outlined';
+import IconButton from "@material-ui/core/IconButton";
+import Brightness4Icon from "@material-ui/icons/Brightness4";
+import Brightness4OutlinedIcon from "@material-ui/icons/Brightness4Outlined";
 
 import { categorySchema } from "@/constant/category";
 
 import { DarkModeContext } from "@/context/darkModeContext";
 
-
 const Header = props => {
-    const {
-        title = "肯游扣部落格 | Yu Ken Code Blog",
-        description = "紀錄自身學習程式歷程，從體能教練轉職成為前端工程師",
-        sharingTitle = "從體能教練轉職前端工程師、不斷自學精進和熱愛用技術去解決身邊的問題｜Yu Ken Code Blog"
-    } = props;
+  const {
+    title = "肯游扣部落格 | Yu Ken Code Blog",
+    description = "紀錄自身學習程式歷程，從體能教練轉職成為前端工程師",
+    sharingTitle = "從體能教練轉職前端工程師、不斷自學精進和熱愛用技術去解決身邊的問題｜Yu Ken Code Blog"
+  } = props;
 
-    const classes = useHeaderStyles();
+  const classes = useHeaderStyles();
 
-    const router = useRouter();
+  const router = useRouter();
 
-    const { isDarkMode, setIsDarkMode } = useContext(DarkModeContext);
+  const { isDarkMode, setIsDarkMode } = useContext(DarkModeContext);
 
-    const handleRouteChange = (path) => {
-        router.push(path)
-    }
+  const handleRouteChange = path => {
+    router.push(path);
+  };
 
-    const handleChangeTheme = () => {
-        setIsDarkMode(!isDarkMode);
-    }
+  const handleChangeTheme = () => {
+    setIsDarkMode(!isDarkMode);
+  };
 
-    const backToHomePage = () => {
-        router.push("/")
-    }
+  const backToHomePage = () => {
+    router.push("/");
+  };
 
-    return (
-        <>
-            <Head>
-                <title>{title}</title>
-                <meta charSet="utf-8" />
-                <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1" />
-                <meta name="description" content={description} />
-                <meta content={sharingTitle} property="og:title" />
-                <meta content={description} property="og:description" />
-                <meta content={sharingTitle} property="twitter:title" />
-                <meta content={description} property="twitter:description" />
-                <meta name="keywords" content="前端工程師、轉職、自學、體能教練" />
-                <meta name="theme-color" content="#317EFB" />
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1"
+        />
+        <meta name="description" content={description} />
+        <meta content={sharingTitle} property="og:title" />
+        <meta content={description} property="og:description" />
+        <meta content={sharingTitle} property="twitter:title" />
+        <meta content={description} property="twitter:description" />
+        <meta name="keywords" content="前端工程師、轉職、自學、體能教練" />
+        <meta name="theme-color" content="#317EFB" />
 
-                <link rel="manifest" href="/manifest.json" />
-                <link href="/icons/favicon-16x16-dunplab-manifest-28429.png" rel="icon" type="image/png" sizes="16x16" />
-                <link href="/icons/favicon-32x32-dunplab-manifest-28429.png" rel="icon" type="image/png" sizes="32x32" />
-                <link href="/icons/apple-icon-60x60-dunplab-manifest-28429.png" rel="apple-touch-icon" type="image/png" sizes="60x60" />
-            </Head>
-            <Grid
-                classes={{
-                    root: classes.gridRoot
-                }}
-                item
-                xs={12}
-            >
-                <Toolbar
-                    classes={{
-                        root: clsx(classes.toolbar, {
-                            [classes.darkMode]: isDarkMode
-                        })
-                    }}
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/icons/favicon-16x16-dunplab-manifest-28429.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/icons/favicon-32x32-dunplab-manifest-28429.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link
+          href="/icons/apple-icon-60x60-dunplab-manifest-28429.png"
+          rel="apple-touch-icon"
+          type="image/png"
+          sizes="60x60"
+        />
+      </Head>
+      <Grid
+        classes={{
+          root: classes.gridRoot
+        }}
+        item
+        xs={12}
+      >
+        <Toolbar
+          classes={{
+            root: classes.toolbar
+          }}
+        >
+          <Grid item xs={6}>
+            <Button onClick={backToHomePage}>
+              <Avatar
+                className={classes.logo}
+                src="/icons/favicon-32x32-dunplab-manifest-28429.png"
+                alt="Ken Code"
+              />
+              <h1 className={classes.logoWord}>Yu Ken Code</h1>
+            </Button>
+          </Grid>
+          <Grid item xs={6}>
+            <div className={classes.menu}>
+              {categorySchema.map(item => (
+                <Button
+                  key={item.id}
+                  classes={{
+                    label: clsx({
+                      [classes.selectedButtonLabel]: item.path.includes(
+                        router.query.category
+                      )
+                    })
+                  }}
+                  onClick={() => handleRouteChange(item.path)}
                 >
-                    <Grid item xs={6}>
-                        <Button onClick={backToHomePage}>
-                            <Avatar className={classes.logo} src="/icons/favicon-32x32-dunplab-manifest-28429.png" alt="Ken Code" />
-                            <h1 className={classes.logoWord}>
-                                Yu Ken Code
-                            </h1>
-                        </Button>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <div className={classes.menu}>
-                            {
-                                categorySchema.map(item => (
-                                    <Button key={item.id} onClick={() => handleRouteChange(item.path)}>
-                                        {item.title}
-                                    </Button>
-                                ))
-                            }
-                            <IconButton onClick={handleChangeTheme}>
-                                {
-                                    isDarkMode ?
-                                        <Brightness4OutlinedIcon /> :
-                                        <Brightness4Icon />
-                                }
-                            </IconButton>
-                        </div>
-                    </Grid>
-                </Toolbar>
-            </Grid>
-        </>
-    )
-}
+                  {item.title}
+                </Button>
+              ))}
+              <IconButton onClick={handleChangeTheme}>
+                {isDarkMode ? <Brightness4OutlinedIcon /> : <Brightness4Icon />}
+              </IconButton>
+            </div>
+          </Grid>
+        </Toolbar>
+      </Grid>
+    </>
+  );
+};
 
-const useHeaderStyles = makeStyles((theme) => ({
-    gridRoot: {
-        position: "sticky",
-        top: 0
-    },
-    toolbar: {
-        transition: "background-color 2s"
-    },
-    darkMode: {
-        "& span, h1": {
-            color: theme.color.word.darkMode
-        }
-    },
-    logo: {
-        "& img": {
-            width: 300
-        }
-    },
-    logoWord: {
-        margin: "0px 0px 0px 10px",
-        padding: 0,
-        fontSize: "1.5em",
-        textTransform: "none"
-    },
-    menu: {
-        textAlign: "end"
+const useHeaderStyles = makeStyles(theme => ({
+  gridRoot: {
+    position: "sticky",
+    top: 0
+  },
+  toolbar: {
+    transition: "background-color 2s"
+  },
+  logo: {
+    "& img": {
+      width: 300
     }
+  },
+  logoWord: {
+    margin: "0px 0px 0px 10px",
+    padding: 0,
+    fontSize: "1.5em",
+    textTransform: "none"
+  },
+  menu: {
+    textAlign: "end"
+  },
+  selectedButtonLabel: {
+    fontWeight: "bold",
+    // ken
+    "&:after": {
+      width: 10,
+      height: 5,
+      backgroundColor: "red"
+    }
+  }
 }));
 
 export default Header;
--- pages/[category]/index.js
+++ pages/[category]/index.js
@@ -1,215 +1,193 @@
-import { useEffect, useContext } from "react";
+import { useContext } from "react";
 import { useRouter } from "next/router";
+import capitalize from "lodash/capitalize";
 import clsx from "clsx";
 
-import Header from "@/components/Header";
 import Banner from "@/components/Banner";
 import SideList from "@/components/SideList";
 
 import { categorySchema } from "@/constant/category";
 
 import { makeStyles } from "@material-ui/core";
-import Paper from '@material-ui/core/Paper';
+import Paper from "@material-ui/core/Paper";
 
 import { DarkModeContext } from "@/context/darkModeContext";
 
 import { getPath, getTitle, getAgenda } from "@/helper/getArticleTag";
 
+const BlogList = props => {
+  const { category = "", allFormatBlogs = [] } = props;
 
-const BlogList = (props) => {
-    const {
-        category = "",
-        allFormatBlogs = []
-    } = props;
+  const classes = useBlogListStyles();
 
-    const classes = useBlogListStyles();
+  const router = useRouter();
 
-    const router = useRouter();
+  const { isDarkMode } = useContext(DarkModeContext);
 
-    const { isDarkMode } = useContext(DarkModeContext);
+  const handleClickArticle = path => {
+    router.push(`${category}/${path}`);
+  };
 
-    const handleClickArticle = (path) => {
-        router.push(`${category}/${path}`)
-    };
+  const BlogList = () => {
+    return allFormatBlogs.map((blog, index) => (
+      <Paper
+        classes={{
+          root: clsx(classes.paperRoot, {
+            [classes.darkMode]: isDarkMode
+          })
+        }}
+        key={index}
+        elevation={0}
+        onClick={() => handleClickArticle(getPath(blog))}
+      >
+        <div className={classes.imgWrapper}>
+          <img
+            className={classes.img}
+            src={`/article-img/${getPath(blog)}.jpg`}
+          />
+        </div>
+        <div className={classes.wordWrapper}>
+          <p className={classes.cardTitle}>{getTitle(blog)}</p>
+          <p
+            className={classes.cardDescription}
+            dangerouslySetInnerHTML={{ __html: getAgenda(blog) }}
+          />
+        </div>
+      </Paper>
+    ));
+  };
 
-    const BlogList = () => {
-        return (
-            allFormatBlogs.map((blog, index) => (
-                <Paper
-                    classes={{
-                        root: clsx(classes.paperRoot, {
-                            [classes.darkMode]: isDarkMode
-                        })
-                    }}
-                    key={index}
-                    elevation={0}
-                    onClick={() => handleClickArticle(getPath(blog))}
-                >
-                    <div className={classes.imgWrapper}>
-                        <img
-                            className={classes.img}
-                            src={`/article-img/${getPath(blog)}.jpg`}
-                        />
-                    </div>
-                    <div className={classes.wordWrapper}>
-                        <p
-                            className={classes.cardTitle}
-                        >
-                            {getTitle(blog)}
-                        </p>
-                        <p
-                            className={classes.cardDescription}
-                            dangerouslySetInnerHTML={{ __html: getAgenda(blog) }}
-                        />
-                    </div>
-                </Paper>
-            ))
-        )
-    }
-
-    return (
-        <>
-            {/* <Header
-                title={`${category.toUpperCase()} | 游肯扣部落格`}
-                description={`${category} 的技術部落格文章列表`}
-                sharingTitle={`${category} 的技術部落格文章列表`}
-            /> */}
-            <Banner
-                word={`${category.toUpperCase()} 系列文章`}
-            />
-            <SideList
-                category={category}
-                list={allFormatBlogs}
-            />
-            <div className={classes.container}>
-                <BlogList />
-            </div>
-        </>
-    )
-}
-
+  return (
+    <>
+      <Banner word={capitalize(category)} />
+      <SideList category={category} list={allFormatBlogs} />
+      <div className={classes.container}>
+        <BlogList />
+      </div>
+    </>
+  );
+};
 
 export const getStaticProps = async context => {
-    const { category } = context.params;
+  const { category } = context.params;
 
-    const fs = require("fs");
-    const hljs = require("highlight.js");
+  const fs = require("fs");
+  const hljs = require("highlight.js");
 
-    const md = require("markdown-it")({
-        html: true,
-        highlight: (str, lang) => {
-            if (lang && hljs.getLanguage(lang)) {
-                try {
-                    return hljs.highlight(lang, str).value;
-                } catch (__) {
+  const md = require("markdown-it")({
+    html: true,
+    highlight: (str, lang) => {
+      if (lang && hljs.getLanguage(lang)) {
+        try {
+          return hljs.highlight(lang, str).value;
+        } catch (__) {}
+      }
 
-                }
-            }
-
-            return "";
-        }
-    });
-
-    // 擋若不是文章列表內的網址列畫面不會噴錯
-    let files;
-
-    try {
-        files = fs.readdirSync(`${process.cwd()}/contents/${category}`, "utf-8");
+      return "";
     }
-    catch (err) {
-        return {
-            props: {
-                category: "",
-                allFormatBlogs: []
-            }
-        }
-    }
+  });
 
-    const allMdFiles = files.filter(fn => fn.endsWith(".md"));
+  // 擋若不是文章列表內的網址列畫面不會噴錯
+  let files;
 
-    const allContent = allMdFiles.map(mdFile => fs.readFileSync(`${process.cwd()}/contents/${category}/${mdFile}`, {
-        encoding: "utf-8",
-    }))
-
-    const allFormatBlogs = allContent.map(content => md.render(content));
-
+  try {
+    files = fs.readdirSync(`${process.cwd()}/contents/${category}`, "utf-8");
+  } catch (err) {
     return {
-        props: {
-            category: context.params.category,
-            allFormatBlogs
-        }
+      props: {
+        category: "",
+        allFormatBlogs: []
+      }
     };
-}
+  }
+
+  const allMdFiles = files.filter(fn => fn.endsWith(".md"));
+
+  const allContent = allMdFiles.map(mdFile =>
+    fs.readFileSync(`${process.cwd()}/contents/${category}/${mdFile}`, {
+      encoding: "utf-8"
+    })
+  );
+
+  const allFormatBlogs = allContent.map(content => md.render(content));
+
+  return {
+    props: {
+      category: context.params.category,
+      allFormatBlogs
+    }
+  };
+};
 
 export const getStaticPaths = async () => {
-    const paths = categorySchema.map(fileName => {
-        return {
-            params: {
-                category: fileName.title
-            }
-        }
-    })
-
+  const paths = categorySchema.map(fileName => {
     return {
-        paths,
-        fallback: true
+      params: {
+        category: fileName.title
+      }
     };
-}
+  });
 
-const useBlogListStyles = makeStyles((theme) => ({
-    container: {
-        marginTop: 30,
-        padding: "0px 24px",
-        display: "flex",
-        flexWrap: "wrap",
-        justifyContent: "space-between",
-        transition: "background-color 2s",
-        maxWidth: 1200
-    },
-    darkMode: {
-        boxShadow: "0 2px 10px 0 rgba(255, 255, 255, 0.1)"
-    },
-    articleSection: {
-        cursor: "pointer",
-        width: "100%"
-    },
-    paperRoot: {
-        display: "flex",
-        flexDirection: "column",
-        marginBottom: 30,
-        width: 350,
-        borderRadius: 8,
-        cursor: "pointer",
-        boxShadow: "0 2px 10px 0 rgba(0, 0, 0, 0.1)",
-        "&:hover": {
-            backgroundColor: "#c0c0c0"
-        }
-    },
-    imgWrapper: {
-        overflow: "hidden",
-        height: 235,
-        borderRadius: "8px 8px 0px 0px"
-    },
-    img: {
-        width: "100%",
-        transition: "all .5s ease",
-        "&:hover": {
-            transform: "scale(1.2)"
-        }
-    },
-    wordWrapper: {
-        paddingBottom: 20,
-        paddingLeft: 20
-    },
-    cardTitle: {
-        fontSize: 20
-    },
-    cardDescription: {
-        margin: 0,
-        "& p, ul": {
-            margin: 0
-        }
+  return {
+    paths,
+    fallback: true
+  };
+};
+
+const useBlogListStyles = makeStyles(theme => ({
+  container: {
+    marginTop: 30,
+    padding: "0px 24px",
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    transition: "background-color 2s",
+    maxWidth: 1200
+  },
+  darkMode: {
+    boxShadow: "0 2px 10px 0 rgba(255, 255, 255, 0.1)"
+  },
+  articleSection: {
+    cursor: "pointer",
+    width: "100%"
+  },
+  paperRoot: {
+    display: "flex",
+    flexDirection: "column",
+    marginBottom: 30,
+    width: 350,
+    borderRadius: 8,
+    cursor: "pointer",
+    boxShadow: "0 2px 10px 0 rgba(0, 0, 0, 0.1)",
+    "&:hover": {
+      backgroundColor: "#c0c0c0"
     }
+  },
+  imgWrapper: {
+    overflow: "hidden",
+    height: 235,
+    borderRadius: "8px 8px 0px 0px"
+  },
+  img: {
+    width: "100%",
+    transition: "all .5s ease",
+    "&:hover": {
+      transform: "scale(1.2)"
+    }
+  },
+  wordWrapper: {
+    paddingBottom: 20,
+    paddingLeft: 20
+  },
+  cardTitle: {
+    fontSize: 20
+  },
+  cardDescription: {
+    margin: 0,
+    "& p, ul": {
+      margin: 0
+    }
+  }
 }));
 
 export default BlogList;
